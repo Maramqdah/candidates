772409620a694aac654047dc433ded0fabb6488d
Maram Qdah <maram.m.qdah@gmail.com>
2021-11-05 17:20:38 -0400

    add functions to filter.rb

--- filters.rb
+++ filters.rb
@@ -1,27 +1,26 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
-
-
 def find(id)
-
-candidate.each do |candidate|
-  if candidate[:id] == id
-    pp candidate
-  else
-    nil
+  # Takes in an id
+  # Returns the candidate with that :id
+  # If there is no candidate with that id, it naturally returns nil
+  @candidates.each do |candidate|
+    if candidate[:id] == id
+      pp candidate
+    else
+      nil
+    end
   end
 end
-    
-end
 
-def experienced?(candidate)
-  # Your code Here
+def ordered_by_qualifications(candidates)
+
   pp candidates.sort_by{ |c| [c[:years_of_experience], c[:github_points]] }
 
 end
 
+
+
+
 def qualified_candidates(candidates)
-  # Your code Here
   candidates.each do |candidate|
     if experienced?(candidate) && github_pt_check?(candidate) && ruby_python_check?(candidate) && age_over_17?(candidate) && application_date?(candidate)
       pp candidate
